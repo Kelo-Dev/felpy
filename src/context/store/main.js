82330ee177ee10b7788b0271716a23604d990823
Travailleur Lorick <loricktravailleur@gmail.com>
2020-10-24 11:06:49 +0200

    setup eslint and prettier

--- src/context/store/main.js
+++ src/context/store/main.js
@@ -1,16 +1,19 @@
-import React, { createContext } from 'react';
+import React, { createContext } from 'react'
 
-import mainReducer from '../reducer/main';
+import mainReducer from '../reducer/main'
 
 /**
  * @type {Object} initialState
  */
-export const initialState = {
-};
+export const initialState = {}
 
-export const MainStore = createContext();
+export const MainStore = createContext()
 
 export default function MainProvider({ children }) {
-  const [state, dispatch] = React.useReducer(mainReducer, initialState);
-  return <MainStore.Provider value={{ state, dispatch }}>{children}</MainStore.Provider>;
+  const [state, dispatch] = React.useReducer(mainReducer, initialState)
+  return (
+    <MainStore.Provider value={{ state, dispatch }}>
+      {children}
+    </MainStore.Provider>
+  )
 }
